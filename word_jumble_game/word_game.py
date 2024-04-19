70e1ad88171d2090c395d818f99bfd5ee86daafc
madefromjames <madefromjames@gmail.com>
2024-04-19 10:32:52 +0100

    bugs

--- word_jumble_game/word_game.py
+++ word_jumble_game/word_game.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, Entry, Frame, END, PhotoImage, LEFT
+from tkinter import Tk, Button, Label, Entry, Frame, END, PhotoImage, LEFT, RIGHT
 import random
 from tkinter import messagebox
 
@@ -50,6 +50,7 @@ def main():
             rand_word = main_words[rand_num]
 
             previous_word = ''
+            lives = '💖💖💖💖💖'
             hint_count = 0
             points = 1
 
@@ -76,16 +77,22 @@ def main():
                     new_shuffled_word = ''.join(current_word)
                 word.config(text=new_shuffled_word)
 
+            # Function too drecement lives and update the display
+            def decrement_lives():
+                nonlocal lives
+                lives = lives[:-1] # Remove one heart from the string
+                live.config(text=lives) # Update the display lives count
+
             # Check the user's input
             def check():
-                nonlocal points, rand_word, hint_count, score, rand_num, shuffled_word
+                nonlocal points, rand_word, hint_count, point, rand_num, shuffled_word, lives
                 user_word = get_input.get().lower()
 
                 # Check if the inputs is correct
                 if user_word == rand_word:
                     # Update score and show message for correct answer
                     points += 5
-                    score.config(text="Score: " + str(points))
+                    point.config(text=f"Points: {str(points)}")
                     random_message = random.choice(["Great job!", "Well done!", "Awesome!", "Excellent!", "Fantastic!", "Amazing work!", "Keep it up!", "Yummy!", "Fresh!"])
                     messagebox.showinfo('Correct ✅', random_message)
                     # Select a new random word
@@ -105,21 +112,18 @@ def main():
                     hint_label.config(text="HINT ▶")
                 elif not get_input.get():
                     # show error message for empty input
-                    messagebox.showerror('Error', 'Empty Word')
+                    messagebox.showwarning('Error', 'Empty Word')
                     get_input.delete(0, END)
                 else:
                     # Handle incorrect input
-                    if points < 2:
-                        show_message("Game Will End if Score Is Below ZERO!")
-                    if points < 1:
-                        messagebox.showerror('Exit', 'Game Over - Better Luck Next Time')
-                        window.destroy()
-                        start_page()
+                    if len(lives) == 1:
+                        show_message(f'High Score: {points}', color='#42f58a')
+                        messagebox.showerror('Error', 'Game Over - Better Luck Next Time')
+                        back_button()
                     else:
                         messagebox.showerror('Error', 'Incorrect Answer')
                         get_input.delete(0, END)
-                        points -= 1
-                        score.config(text=f"Score: {str(points)}")
+                        decrement_lives() # Decrement lives and update the display
 
             # Function to show a hint for the word
             def show_hint(word, count):
@@ -133,6 +137,7 @@ def main():
 
                 if count < word_length:
                     for answer in main_words:
+                        # Compare word's letter and length to all element in main_words
                         if len(answer) == word_length and all(letter in answer for letter in word):
                             if points < 1:
                                 show_message("Not enough points!")
@@ -140,7 +145,7 @@ def main():
                                 hint_label.config(text=f"{hint_label['text']} {answer[hint_count].upper()}")
                                 hint_count += 1
                                 points -= 1
-                            score.config(text=f"Score: {str(points)}")
+                            point.config(text=f"Points: {str(points)}")
                             break
 
             # Function to show information message
@@ -151,15 +156,15 @@ def main():
             
             # FUnction to skip/change word
             def skip_word():
-                nonlocal rand_num, points, score, previous_word, rand_word
+                nonlocal rand_num, points, previous_word, rand_word
                 previous_word = rand_word
                 response = messagebox.askyesno('Warning', 'You will lose 4 points. Do you wish to proceed?')
                 if response:
-                    if points < 5:
+                    if points < 4:
                         show_message("Not enough points!")
                         return
                     points -= 4
-                    score.config(text=f"Score: {str(points)}")
+                    point.config(text=f"Points: {str(points)}")
                     # Select a new random word
                     rand_num = random.randrange(0, len(main_words))
                     rand_word = main_words[rand_num]
@@ -176,23 +181,28 @@ def main():
                     hint_label.config(text="HINT ▶")
                     show_message(f"Previous Answer: {previous_word.upper()}")
 
-            # FUnction to go back to start page
+            # Function to go back to start page
             def back_button():
                 # Destroy the current window and show start page
                 window.destroy()
                 start_page()
+                
 
             # Navigation frame for button and score
             nav_label = Frame(window, bg="#040402")
             nav_label.pack(fill='x')
 
             # Back button
-            back_btn = Button(nav_label, image=img1, bg="#040402", border=0, justify='center', command=back_button)
+            back_btn = Button(nav_label, image=img1, bg="#040402", border=0, cursor="hand2", command=back_button)
             back_btn.pack(side=LEFT, padx=(10, 85))
             
             # Score label
-            score = Label(nav_label, text=f"Score: {str(points)}", pady=10, bg="#040402", fg="#decac0", font="Titillium 13 bold")
-            score.pack(side=LEFT, padx=(85, 10))
+            point = Label(nav_label, text=f"Points: {str(points)}", pady=15, bg="#040402", fg="#decac0", font="Titillium 13 bold")
+            point.pack(side=LEFT, padx=(85, 10))
+
+            # Lives label
+            live = Label(nav_label, text=lives, bg="#040402", fg="#eb3462", font="Titillium 18 bold")
+            live.pack(side=RIGHT, padx=(0, 10))
 
             # Display the shuffled word
             word = Label(window, text=shuffled_word, pady=10, bg="#040402", fg="#decac0", font="Titillium 35 bold")
@@ -215,29 +225,76 @@ def main():
             button_frame.pack(pady=20)  # Pack the frame with some padding
 
             # Shuffle button
-            shuffle = Button(button_frame, text="🔀 Shuffle", width=14, bd=4, font=("", 13), bg="#ad8d76", command=reshuffle_btn)
+            shuffle = Button(button_frame, text="🔀 Shuffle", width=14, bd=4, font=("", 13), bg="#ad8d76", cursor="hand2", command=reshuffle_btn)
             shuffle.grid(row=0, column=0, padx=10, pady=10)  # Pack the shuffle button to the left with padding
 
             # Submit button
-            submit = Button(button_frame, text="✅ Submit", width=14, bd=4, font=("", 13), bg="#ad8d76", command=check)
+            submit = Button(button_frame, text="✅ Submit", width=14, bd=4, font=("", 13), bg="#ad8d76", cursor="hand2", command=check)
             submit.grid(row=0, column=1, padx=10, pady=10)  # Pack the submit button to the right with padding
             window.bind('<Return>', lambda event=None: submit.invoke()) # Bind keyboard's ENTER button to submit button
 
             # Hint button
-            hint = Button(button_frame, text="ℹ️ Hint", width=14, bd=4, font=("", 13), bg="#ad8d76", command= lambda: show_hint(word.cget("text"), hint_count))
+            hint = Button(button_frame, text="ℹ️ Hint", width=14, bd=4, font=("", 13), bg="#ad8d76", cursor="hand2", command= lambda: show_hint(word.cget("text"), hint_count))
             hint.grid(row=1, column=0, padx=10, pady=10)  # Pack the hint button at the top with padding
 
             # Skip button
-            skip = Button(button_frame, text="⏩ Skip", width=14, bd=4, font=("", 13), bg="#ad8d76", command=skip_word)
+            skip = Button(button_frame, text="⏩ Skip", width=14, bd=4, font=("", 13), bg="#ad8d76", cursor="hand2", command=skip_word)
             skip.grid(row=1, column=1, padx=10, pady=10)  # Pack the skip button at the top with padding
 
             window.mainloop()
+
+        def help_info():
+            # Create the main window
+            help_window = Tk()
+            help_window.geometry("500x500+500+100")
+            help_window.resizable(0, 0)
+            help_window.title("Word Game")
+            help_window.configure(background="#040402")
+            help_window.iconbitmap(r'wordicon.ico')
+
+            # Function to go back to start page
+            def back_button():
+                # Destroy the current window and show start page
+                help_window.destroy()
+                start_page()
+
+            # Load the back button image
+            img1 = PhotoImage(file="back-btn.png")
+
+            # Back button
+            back_btn = Button(help_window, image=img1, bg="#040402", border=0, cursor="hand2", command=back_button)
+            back_btn.pack()
+
+            instruction_label = Label(help_window, text="""How to Play
+1. OBJECTIVES: Guess the shuffled word correctly to earn points.
+
+2. SHUFFLE BUTTON: Use this button to reshuffle the letters of the word if you're stuck.
+
+3. HINT BUTTON: Get a hint for the word by clicking this button. Each hint deducts a point from your score.
+
+4. SKIP BUTTON: Skip a word if you're unable to guess it. Skipping deducts 4 points from your score.
+
+5. SUBMIT BUTTON: Enter your guess in the input field and click this button to check if it's correct.
+
+6. LIVES: You start with 5 lives represented by heart emojis (💖). Each incorrect guess deducts one life.
+
+7. POINTS: Earn points for correct guesses. Your current score is displayed at the top.
+                                  
+8. HIGH SCORES: Your highest score achieved in the game will be displayed when the game ends.
+                                          
+Have fun and good luck! 🍀""", bg="#040402", fg="#decac0", font="Titillium 11 bold", justify=LEFT, wraplength=470, anchor="nw")
+            instruction_label.pack(fill="both", padx=10)
+
+            help_window.mainloop()
             
         # Function to show main game window
-        def show_game():
+        def show_game(button):
             # Destroy the start page and start the main game
             main_window.destroy()
-            main_game()
+            if button == "start":
+                main_game()
+            elif button == "help":
+                help_info()
 
         # Create the main window for the start page
         main_window = Tk()
@@ -267,12 +324,16 @@ def main():
         names_label.pack(side=LEFT)
 
         # Start button
-        start_btn = Button(main_window, text="Start", width=14, bd=4, bg="#ad8d76", font=("", 10, 'bold',), cursor="hand2", command=show_game)
-        start_btn.pack(pady=(35, 10))
+        start_btn = Button(main_window, text="Start", width=14, bd=4, bg="#ad8d76", font=("", 10, 'bold',), cursor="hand2", command=lambda: show_game("start"))
+        start_btn.pack(pady=(35, 5))
+
+        # How to play button
+        help_btn = Button(main_window, text="How to play", width=14, bd=4, bg="#ad8d76", font=("", 10, 'bold',), cursor="hand2", command=lambda: show_game("help"))
+        help_btn.pack(pady=5)
 
         # Exit button
         exit_btn = Button(main_window, text="Exit", width=14, bd=4, bg="#ad8d76", font=("", 10, 'bold',), cursor="hand2", command=lambda: main_window.destroy() if messagebox.askyesno('Exit', 'Are you sure you want to exit?') else None)
-        exit_btn.pack(pady=(10))
+        exit_btn.pack(pady=5)
 
         main_window.mainloop()
 
